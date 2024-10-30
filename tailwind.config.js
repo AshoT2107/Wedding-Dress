c90edcbfabb1cfa3669401a4525b8d620c5c98be
AshoT2107 <ashotm1999@gmail.com>
2024-10-30 11:21:55 +0500

    wedding dress

---
+++ tailwind.config.js
@@ -0,0 +1,19 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./*.html"],
+  theme: {
+    extend: {
+      // fontFamily: {
+      //   hind: ['"Hind Vadodara"', "sans-serif"],
+      // },
+      // fontWeight: {
+      //   light: 300,
+      //   normal: 400,
+      //   medium: 500,
+      //   semibold: 600,
+      //   bold: 700,
+      // },
+    },
+  },
+  plugins: [],
+};
